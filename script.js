0313612d1f2a0094f2b484bdd9df70359e7a9f54
ikenna nwaka <inwaka39@gmail.com>
2025-05-21 19:25:49 +0200

    else

--- script.js
+++ script.js
@@ -92,5 +92,7 @@ const playSong = (id) => {
 
     if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
     audio.currentTime = 0;
+  } else {
+    audio.currentTime = userData?.songCurrentTime;
   }
 }
