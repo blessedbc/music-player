c5373a31431bd29f5a7f329fe934c28290c7dfad
ikenna nwaka <inwaka39@gmail.com>
2025-05-23 17:44:18 +0200

    setPlayerDisplay

--- script.js
+++ script.js
@@ -158,3 +158,13 @@ const deleteSong = (id) => {
   setPlayButtonAccessibleText(); 
 
 };
+
+const setPlayerDisplay = () => {
+  const playingSong = document.getElementById("player-song-title");
+  const songArtist = document.getElementById("player-song-artist");
+  const currentTitle = userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
+
+  playingSong.textContent = currentTitle ? currentTitle : "";
+  songArtist.textContent = currentArtist ? currentArtist : "";
+};
